111b6544f61e309e0873bdf39cb66ab32a8e668c
Zach Rose <zach@windmountainsoftware.com>
2024-09-19 20:17:59 -0400

    Aggregate phonebook listings into reliable view of former locations (mcghosts)

--- mapping/scripts/load_mcghosts.sh
+++ mapping/scripts/load_mcghosts.sh
@@ -6,28 +6,49 @@ ogr2ogr -f "PostgreSQL" \
   -oo X_POSSIBLE_NAMES=lon \
   -oo Y_POSSIBLE_NAMES=lat \
   -s_srs "EPSG:4326" \
-  -nln mcghost \
+  -nln raw_phonebook_listing \
   -t_srs "EPSG:4326" \
   -lco GEOM_TYPE=geography \
   -lco OVERWRITE=YES;
 
-# psql -h 127.0.0.1 -d mctrot << SQL
-# 
-#   CREATE TABLE phonebook_listing_raw (id SERIAL PRIMARY KEY, year text, phonebook text, name text, address text, phone varchar(18), latitude float, longitude float);
-# 
-#   COPY phonebook_listing_raw(year, phonebook, name, address, phone, latitude, longitude) FROM '/Users/zachrose/mctrot/mapping/data/mcghosts_geocoded.csv' WITH DELIMITER ',';
-# 
-#   # CREATE OR REPLACE VIEW phonebook_listing AS SELECT raw.year, raw.phonebook, raw.address, COUNT (raw.address) AS phone_number_count FROM phonebook_listing_raw as raw GROUP BY year, phonebook, address order by year, address;
-# 
-#   # count unique addresses by year
-#   # SELECT l.year, l.phonebook, COUNT(l.phonebook), REPEAT('#', count(l.phonebook)::int) AS histogram FROM phonebook_listing AS l GROUP BY year, phonebook ORDER BY year, phonebook;
-# 
-# SQL
+psql -h 127.0.0.1 -d mctrot << SQL
+  CREATE OR REPLACE VIEW
+      phonebook_listing AS
+  SELECT
+      raw_phonebook_listing.*,
+      CAST(SPLIT_PART(year, '-', 1) AS int) AS min_year,
+      CAST(
+          COALESCE(NULLIF(SPLIT_PART(year, '-', 2), ''), year) AS int
+      ) AS max_year
+  FROM
+      raw_phonebook_listing
+      JOIN borough ON ST_CONTAINS (
+          borough.the_geog::geometry,
+          raw_phonebook_listing.the_geog::geometry
+      )
+      AND borough.boro_name = 'Manhattan';
+SQL
 
-# psql -h 127.0.0.1 -d mctrot << SQL
-#   CREATE OR REPLACE VIEW mcisland_mcd AS
-#   SELECT mcd.* FROM mcd
-#   JOIN borough ON ST_CONTAINS(borough.the_geog::geometry, mcd.the_geog::geometry)
-#   AND borough.boro_name = 'Manhattan'
-# SQL
-    
+psql -h 127.0.0.1 -d mctrot << SQL
+  CREATE OR REPLACE VIEW
+      mcghost AS
+  SELECT DISTINCT
+      (l.address),
+      min(l.min_year) OVER by_address AS first_seen,
+      max(l.max_year) OVER by_address AS last_seen,
+      l.the_geog,
+      mcd.id
+  FROM
+      phonebook_listing l
+  LEFT JOIN mcisland_mcd mcd ON ST_DISTANCE(mcd.the_geog::geometry, l.the_geog::geometry) < 0.0015
+  WHERE mcd.id IS NULL
+  WINDOW
+      by_address AS (
+          PARTITION BY
+              address
+      )
+  ORDER BY
+      first_seen,
+      address;
+SQL
+
