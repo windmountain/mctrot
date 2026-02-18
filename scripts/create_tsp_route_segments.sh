5f8559aed970108ed96de5ac86d5cce8ba8e5b0a
Zach Rose <zach@windmountainsoftware.com>
2026-02-18 11:52:23 -0500

    Get finished route down to individual street level

--- scripts/create_tsp_route_segments.sh
+++ scripts/create_tsp_route_segments.sh
@@ -11,12 +11,22 @@ SELECT
   a.addressline1  AS from_address,
   b.mcd_id        AS to_mcd_id,
   b.addressline1  AS to_address,
-  COALESCE(r1.the_geog, r2.the_geog) AS the_geog
+  sr.st_label,
+  sr.full_stree,
+  sr.the_geom::geography AS the_geog
 FROM tsp_route a
 JOIN tsp_route b
   ON b.stop_order = (a.stop_order % (SELECT MAX(stop_order) FROM tsp_route)) + 1
-LEFT JOIN route r1 ON r1.from_mcd_id = a.mcd_id AND r1.to_mcd_id = b.mcd_id
-LEFT JOIN route r2 ON r2.from_mcd_id = b.mcd_id AND r2.to_mcd_id = a.mcd_id;
+JOIN mcd_routing_node src ON src.mcd_id = a.mcd_id
+JOIN mcd_routing_node dst ON dst.mcd_id = b.mcd_id
+JOIN route_dijkstra rd
+  ON  rd.edge != -1
+  AND (
+    (rd.start_vid = src.vertex_id AND rd.end_vid = dst.vertex_id)
+    OR
+    (rd.start_vid = dst.vertex_id AND rd.end_vid = src.vertex_id)
+  )
+JOIN sidewalk_routing sr ON sr.id = rd.edge;
 
 CREATE INDEX tsp_route_segments_geog_idx ON tsp_route_segments USING gist(the_geog);
 
